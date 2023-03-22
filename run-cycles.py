b25d351ad2a914886467dcc62bc691e7d0de8f58
Yuning Shi <shiyuning@gmail.com>
2023-03-21 23:24:55 -0400

    :sparkles: Run Cycles for each administrative region

--- run-cycles.py
+++ run-cycles.py
@@ -4,12 +4,9 @@ import argparse
 import numpy as np
 import os
 import pandas as pd
-import re
 import subprocess
-import sys
 from datetime import datetime
 from io import StringIO
-from pathlib import Path
 from string import Template
 
 """Run Cycles simulations for different crops under different nuclear war scenarios
@@ -17,122 +14,105 @@ from string import Template
 Run Cycles simulations
 """
 
-SEVEN_ZIP = "7zzs"
-START_YEAR = "0005"
-END_YEAR = "0019"
-LUIDS = ["10", "11", "12", "20", "30", "40"]
-CYCLES = "./bin/Cycles"
+SEVEN_ZIP = '7zzs'
+START_YEAR = '0005'
+END_YEAR = '0019'
+CYCLES = './bin/Cycles'
 BASE_TMP = 6.0
 MAX_TMPS = {
-    "Maize": "-999",
-    "SpringWheat": "-999",
-    "WinterWheat": "15.0",
+    'maize': '-999',
+    'springwheat': '-999',
+    'winterwheat': '15.0',
 }
 MIN_TMPS = {
-    "Maize": "12.0",
-    "SpringWheat": "5.0",
-    "WinterWheat": "-999",
+    'maize': '12.0',
+    'springWheat': '5.0',
+    'winterWheat': '-999',
 }
 CROPS = {
-    "Maize": "CornRM.90",
-    "SpringWheat": "SpringWheat",
-    "WinterWheat": "WinterWheat",
+    'maize': 'CornRM.90',
+    'springwheat': 'SpringWheat',
+    'winterwheat': 'WinterWheat',
 }
 MATURITY_TTS = {
-    "Maize": {
-        "CornRM.115": 2425,
-        "CornRM.110": 2300,
-        "CornRM.105": 2175,
-        "CornRM.100": 2050,
-        "CornRM.95": 1925,
-        "CornRM.90": 1800,
-        "CornRM.85": 1675,
-        "CornRM.80": 1550,
-        "CornRM.75": 1425,
-        "CornRM.70": 1300,
+    'maize': {
+        'CornRM.115': 2425,
+        'CornRM.110': 2300,
+        'CornRM.105': 2175,
+        'CornRM.100': 2050,
+        'CornRM.95': 1925,
+        'CornRM.90': 1800,
+        'CornRM.85': 1675,
+        'CornRM.80': 1550,
+        'CornRM.75': 1425,
+        'CornRM.70': 1300,
     }
 }
-
 DOYS = {
-    "1": [1, 31],
-    "2": [32, 59],
-    "3": [60, 90],
-    "4": [91, 120],
-    "5": [121, 151],
-    "6": [152, 181],
-    "7": [182, 212],
-    "8": [213, 243],
-    "9": [244, 273],
-    "10": [274, 304],
-    "11": [305, 334],
-    "12": [335, 365],
+    '1': [1, 31],
+    '2': [32, 59],
+    '3': [60, 90],
+    '4': [91, 120],
+    '5': [121, 151],
+    '6': [152, 181],
+    '7': [182, 212],
+    '8': [213, 243],
+    '9': [244, 273],
+    '10': [274, 304],
+    '11': [305, 334],
+    '12': [335, 365],
 }
+SCENARIOS = [
+    'nw_cntrl_03',
+    'nw_targets_01',
+    'nw_targets_02',
+    'nw_targets_03',
+    'nw_targets_04',
+    'nw_targets_05',
+    'nw_ur_150_07',
+]
+LOOKUP = lambda crop: f'./data/{crop}_rainfed_eow_lookup_3.0.csv'
+SOIL_ARCHIVE = lambda crop: f'./data/{crop}_rainfed_global_soil_3.0.7z'
+WEATHER_ARCHIVE = lambda scenario: f'./data/{scenario}.zip'
+
 
 def calculate_months_for_planting(weather, tmp_max, tmp_min):
     """Calculate months in which crops can be planted
     """
-    tmp_max = float(tmp_max) if tmp_max != "-999" else 100  # -999 is the special case that disables max temperature
+    tmp_max = float(tmp_max) if tmp_max != '-999' else 100  # -999 is the special case that disables max temperature
     tmp_min = float(tmp_min)
 
     # Read weather file with comment lines removed
-    with open(f"input/weather/{weather}", "r") as fp:
-        weather_str = [line.strip() for line in fp if line.strip() and line.strip()[0] != "#"]
+    with open(f'input/weather/{weather}', 'r') as fp:
+        weather_str = [line.strip() for line in fp if line.strip() and line.strip()[0] != '#']
 
     # Read weather file into dataframe
-    df = pd.read_csv(StringIO("\n".join(weather_str[3:])),
+    df = pd.read_csv(StringIO('\n'.join(weather_str[3:])),
         delim_whitespace=True,
         na_values=[-999])
 
     # Calculate month, average temperature, 7-day moving average temperature, thermal time
-    df["month"] = df.apply(lambda x: datetime.strptime("2009-" + "%d" %(x["DOY"]), "%Y-%j").strftime("%m"), axis=1)
-    df["tavg"] = 0.5 * df["TX"] + 0.5 * df["TN"]
-    df["tt"] = df.apply(lambda x: 0.0 if x["tavg"] < BASE_TMP else x["tavg"] - BASE_TMP, axis=1)
-    df["tma"] = df.rolling(7, center=True, min_periods=1).mean()["tavg"]
+    df['month'] = df.apply(lambda x: datetime.strptime('2009-' + '%d' %(x['DOY']), '%Y-%j').strftime('%m'), axis=1)
+    df['tavg'] = 0.5 * df['TX'] + 0.5 * df['TN']
+    df['tt'] = df.apply(lambda x: 0.0 if x['tavg'] < BASE_TMP else x['tavg'] - BASE_TMP, axis=1)
+    df['tma'] = df.rolling(7, center=True, min_periods=1).mean()['tavg']
 
     # Calculate average thermal time
-    tt = df["tt"].sum() / (len(df) / 365.0)
+    tt = df['tt'].sum() / (len(df) / 365.0)
 
     # Filter out days outside allowed temperature range
-    df = df[(df["tma"] > tmp_min) & (df["tma"] < tmp_max)]
+    df = df[(df['tma'] > tmp_min) & (df['tma'] < tmp_max)]
 
     # Return a list months that contain days inside temperature range
-    return df["month"].unique(), tt
+    return df['month'].unique(), tt
 
 
-def convert_soil(soil):
-    """Convert soil files in database to latest Cycles format
-    """
-    NO3 = ["10", "10", "7", "4", "4", "4"]
+def find_optimal_planting_dates(gid, months):
+    """Find optimal planting months/dates for each gid
 
-    # Read original soil file
-    with open(f"input/{soil}", "r") as fp:
-        soil_str = [line.strip() for line in fp if line.strip() and line.strip()[0] != "#"]
-
-    # Write new soil file
-    with open(f"input/soil/{soil}", "w") as fp:
-        fp.write(soil_str[0] + "\n")    # Curve number line
-        fp.write(soil_str[1] + "\n")    # Slope line
-        fp.write(soil_str[2] + "\n")    # Total number of layers line
-        ## Header line
-        tmp = soil_str[3].split()[0:8]
-        tmp.append("SON")   # Add SON header
-        tmp.extend(soil_str[3].split()[8:10])
-        tmp.extend(["BYP_H", "BYP_V"])  # Add bypass headers
-        fp.write("\t".join(tmp) + "\n")
-        ## Layers
-        for kline in range(4, len(soil_str)):
-            tmp = soil_str[kline].split()[0:8]
-            tmp.append("-999")  # Add SON
-            tmp.append(NO3[int(soil_str[kline].split()[0]) - 1])    # Add NO3
-            tmp.append("1")  # Add NH4
-            tmp.extend(["0.0", "0.0"])  # Add bypass parameters
-            fp.write("\t".join(tmp) + "\n")
-
-
-def find_optimal_planting_dates(grid, months):
-    """Find optimal planting months/dates for each grid
     Read season files from 12 planting months and find the month with the best yield with a X-month moving window
-    average"""
+    average
+    """
     yield_avg = np.zeros(12)
 
     # Read season files for each month
@@ -140,18 +120,18 @@ def find_optimal_planting_dates(grid, months):
         ## Read season file
         try:
             df = pd.read_csv(
-                f"output/{grid}_M{month}/season.txt",
-                sep="\t",
+                f'output/{gid}_M{month}/season.txt',
+                sep='\t',
                 header=0,
                 skiprows=[1],
                 skipinitialspace=True,
             )
-            df = df.rename(columns=lambda x: x.strip().lower().replace(" ", "_"))
+            df = df.rename(columns=lambda x: x.strip().lower().replace(' ', '_'))
             ## Filter out the last year (when planting late in the year, crop may not be harvested which causes a bias
             ## towards early in the year)
-            df["year"] = df["plant_date"].str[0:4]
-            df = df[df["year"] != "END_YEAR"]
-            yield_avg[int(month) - 1] = df["grain_yield"].sum()
+            df['year'] = df['plant_date'].str[0:4]
+            df = df[df['year'] != 'END_YEAR']
+            yield_avg[int(month) - 1] = df['grain_yield'].sum()
         except:
             continue
 
@@ -185,8 +165,8 @@ def find_optimal_planting_dates(grid, months):
     # Run Cycles again with spin-up
     cmd = [
         CYCLES,
-        "-bs",
-        f"{grid}_M{ref_month}",
+        '-bs',
+        f'{gid}_M{"%2.2d" % (ref_month)}',
     ]
     subprocess.run(
         cmd,
@@ -195,144 +175,128 @@ def find_optimal_planting_dates(grid, months):
     )
 
     # Return season file with best yield
-    df = pd.read_csv("output/%s_M%2.2d/season.txt" % (grid, ref_month),
-        sep="\t",
+    df = pd.read_csv(
+        'output/%s_M%2.2d/season.txt' % (gid, ref_month),
+        sep='\t',
         header=0,
         skiprows=[1],
         skipinitialspace=True,
     )
-    df = df.rename(columns=lambda x: x.strip().lower().replace(" ", "_"))
+    df = df.rename(columns=lambda x: x.strip().lower().replace(' ', '_'))
     df['crop'] = df['crop'].str.strip()
-    df.insert(0, "grid", grid)
+    df.insert(0, 'gid', gid)
 
     return df
 
 
 def run_cycles(params):
     # Create input directories
-    os.makedirs("input/soil", exist_ok=True)
-    os.makedirs("input/weather", exist_ok=True)
-    os.makedirs("summary", exist_ok=True)
+    os.makedirs('input/soil', exist_ok=True)
+    os.makedirs('input/weather', exist_ok=True)
+    os.makedirs('summary', exist_ok=True)
 
     first = True
 
-    tmp_max = MAX_TMPS[params["crop"]]
-    tmp_min = MIN_TMPS[params["crop"]]
+    tmp_max = MAX_TMPS[params['crop']]
+    tmp_min = MIN_TMPS[params['crop']]
 
-    # Read in look up table
-    frames = []
-    for id in LUIDS:
-        frames.append(pd.read_csv(f"data/{params['crop']}_{id}_v9_Lookup.csv"))
-    df = pd.concat(frames)
+    # Read in look-up table
+    df = pd.read_csv(
+        LOOKUP(params['crop']),
+    )
 
-    # Get a list of grids
-    grids = df["CLM_grid"].unique()
-    df.set_index(["CLM_grid"], inplace=True)
+    # Run each region
+    for _, row in df.iterrows():
+        gid = row['GID']
 
-    # Run each grid
-    for grid in grids:
-        ## Unzip weather file
-        weather = f"{params['scenario']}_{grid}.weather"
+        ## Get weather file from archive
+        weather = f'{params["scenario"]}_{row["Weather"]}.weather'
+        #cmd = [
+        #    'unzip',
+        #    '-oj',
+        #    WEATHER_ARCHIVE(params['scenario']),
+        #    f'{params["scenario"]}/{weather}',
+        #    '-d',
+        #    './input/weather/'
+        #]
+        #subprocess.run(
+        #    cmd,
+        #    stdout=subprocess.DEVNULL,
+        #    stderr=subprocess.DEVNULL,
+        #)
+        if not os.path.exists(f'input/weather/{weather}'):
+            print(f'Skip {gid} due to unavailable weather file')
+            continue
 
-        cmd = [
-            "unzip",
-            "-oj",
-            f"data/{params['scenario']}.zip",
-            f"{params['scenario']}/{weather}",
-            "-d",
-            "./input/weather/"
-        ]
-        subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        ## Get soil file from archive
+        soil = row['Soil']
+        #cmd = [
+        #    SEVEN_ZIP,
+        #    'e',
+        #    SOIL_ARCHIVE(params['crop']),
+        #    '-oinput/soil',
+        #    soil,
+        #    '-aoa',     # Overwrite without prompt
+        #]
+        #subprocess.run(
+        #    cmd,
+        #    stdout=subprocess.DEVNULL,
+        #    stderr=subprocess.DEVNULL,
+        #)
 
-        ## Some locations in the lookup table are sea points in the CLM grids, which do not have weather files. If so,
-        ## skip the grid
-        if not os.path.exists(f"input/weather/{weather}"):
-            print(f"Skip {grid} due to unavailable weather file")
+        if not os.path.exists(f'input/soil/{soil}'):
+            print(f'Skip {gid} due to unavailable soil file')
             continue
 
         ## Find which months are potentially suitable to plant crops. Calculate thermal times for choosing RM
         months, tt = calculate_months_for_planting(weather, tmp_max, tmp_min)
 
         if len(months) == 0:
-            print(f"Skip {grid} due to climate")
+            print(f'Skip {gid} due to climate')
             continue
 
+        print(f'{gid} - weather: {weather}, soil: {soil}')
+
         # Find which RM to be planted
-        crop, _ = min(MATURITY_TTS[params["crop"]].items(), key=lambda x: abs(tt * 0.85 - x[1]))
+        crop, _ = min(MATURITY_TTS[params['crop']].items(), key=lambda x: abs(tt * 0.85 - x[1]))
 
         # Create operation files
         for month in range(1, 13):
-            with open(f"data/template.operation") as op_file:
+            with open(f'data/template.operation') as op_file:
                 op_src = Template(op_file.read())
                 op_data = {
-                    "doy_start": DOYS[str(month)][0],
-                    "doy_end": DOYS[str(month)][1],
-                    "max_tmp": tmp_max,
-                    "min_tmp": tmp_min,
-                    "crop": crop,
+                    'doy_start': DOYS[str(month)][0],
+                    'doy_end': DOYS[str(month)][1],
+                    'max_tmp': tmp_max,
+                    'min_tmp': tmp_min,
+                    'crop': crop,
                 }
                 result = op_src.substitute(op_data)
-                with open("./input/M%2.2d.operation" % (month), "w") as f:
-                    f.write(result + "\n")
-
-        ## Unzip soil file
-        n = len(df.loc[grid].shape)
-        for kloc in range(n):
-            if n == 1:  # Only one location in the grid
-                soil = df.loc[grid]["major_soil_file"]
-            else:
-                soil = df.loc[grid]["major_soil_file"][kloc]
-
-            ### Get land use ID because different land uses are contained in different soil archives
-            luid = re.search(f"{params['crop']}_v(.*?)_", soil).group(1)
-
-            cmd = [
-                SEVEN_ZIP,
-                "e",
-                f"data/{params['crop']}{luid}_v9_major.7z",
-                "-oinput",
-                soil,
-                "-aoa",     # Overwrite without prompt
-            ]
-            subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-
-            ### If soil file exists, exit the loop. Otherwise, test the next location until a soil file is found
-            if os.path.exists(f"input/{soil}"):
-                break
-
-        ## Test if a soil file has been provided. If not, skip the location
-        if not os.path.exists(f"input/{soil}"):
-            print(f"Skip {grid} due to unavailable soil file")
-            continue
-        else:
-            print(f"Grid {grid}, soil file: {soil}")
-
-        ## Convert soil files to latest Cycles format
-        convert_soil(soil)
+                with open('./input/M%2.2d.operation' % (month), 'w') as f:
+                    f.write(result + '\n')
 
         ## Run each month
         for month in months:
             ### Create control file
-            with open(f"data/template.ctrl") as ctrl_file:
+            with open(f'data/template.ctrl') as ctrl_file:
                 ctrl_src = Template(ctrl_file.read())
                 ctrl_data = {
-                    "start": START_YEAR,
-                    "end": END_YEAR,
-                    "operation": f"M{month}.operation",
-                    "soil": "soil/%s" % (soil),
-                    #"soil": "tmp/GenericHagerstown.soil",
-                    "weather": f"weather/{weather}",
+                    'start': START_YEAR,
+                    'end': END_YEAR,
+                    'operation': f'M{month}.operation',
+                    'soil': f'soil/{soil}',
+                    'weather': f'weather/{weather}',
                 }
 
                 result = ctrl_src.substitute(ctrl_data)
-                with open(f"./input/{grid}_M{month}.ctrl", "w") as f:
-                    f.write(result + "\n")
+                with open(f'./input/{gid}_M{month}.ctrl', 'w') as f:
+                    f.write(result + '\n')
 
             ### Run Cycles
             cmd = [
                 CYCLES,
-                "-b",
-                f"{grid}_M{month}",
+                '-b',
+                f'{gid}_M{month}',
             ]
             result = subprocess.run(
                 cmd,
@@ -340,47 +304,67 @@ def run_cycles(params):
                 stderr=subprocess.DEVNULL,
             )
             if result.returncode != 0:
-                print(f"Error running {grid}_M{month}")
+                print(f'Error running {gid}_M{month}')
                 continue
 
         ## Read season files and find optimal planting months
-        exdf = find_optimal_planting_dates(grid, months)
+        exdf = find_optimal_planting_dates(gid, months)
 
         if exdf.empty:
-            print(f"No yield from {grid}.")
+            print(f'No yield from {gid}.')
             continue
 
-        output_file = f"summary/{params['scenario']}_{params['crop']}.txt"
+        output_file = f'summary/{params["scenario"]}_{params["crop"]}.txt'
         if first:
             exdf.to_csv(output_file, index=False)
             first = False
         else:
-            exdf.to_csv(output_file, mode="a", header=False, index=False)
+            exdf.to_csv(output_file, mode='a', header=False, index=False)
 
-        ## Remove generated control files and soil files
-        cmd = "rm input/*.soil input/*.ctrl"
-        subprocess.run(cmd, shell="True")
-
-        # Remove operation files
-        cmd = "rm input/*.operation"
-        subprocess.run(cmd, shell="True")
-
-        ## Remove generated soil files and weather files
-        cmd = "rm input/soil/* input/weather/*"
-        subprocess.run(cmd, shell="True")
+        ## Remove generated control files
+        cmd = 'rm input/*.ctrl input/*.soil'
+        subprocess.run(
+            cmd,
+            shell='True',
+        )
 
         ## Remove output files
-        cmd = "rm -r output/*"
-        subprocess.run(cmd, shell="True")
+        cmd = 'rm -r output/*'
+        subprocess.run(
+            cmd,
+            shell='True',
+        )
+
+    ## Remove operation files
+    cmd = 'rm input/*.operation'
+    subprocess.run(cmd, shell='True')
+
+    ### Remove generated soil files and weather files
+    #cmd = 'rm input/soil/* input/weather/*'
+    #subprocess.run(
+    #    cmd,
+    #    shell='True',
+    #)
 
 
 def _main():
-    parser = argparse.ArgumentParser(description="Cycles execution for a crop")
-    parser.add_argument("--crop", default="Maize", help="Crop to be simulated")
-    parser.add_argument("--scenario", default="nw_cntrl_03", help="NW scenario")
+    parser = argparse.ArgumentParser(description='Cycles execution for a crop')
+    parser.add_argument(
+        '--crop',
+        default='maize',
+        choices=list(CROPS.keys()),
+        help='Crop to be simulated',
+    )
+    parser.add_argument(
+        '--scenario',
+        default='nw_cntrl_03',
+        choices=SCENARIOS,
+        help='NW scenario',
+    )
     args = parser.parse_args()
     run_cycles(vars(args))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     _main()
+
